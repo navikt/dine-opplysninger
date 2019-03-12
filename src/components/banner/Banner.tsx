a9cdcf8887dddbe9d8e6eae91567364d3552afbc
Joselito Poblete <joselito.poblete@gmail.com>
2019-03-12 09:21:33 +0100

    FO-1977 Endre tekst i banner

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -6,7 +6,7 @@ function Banner () {
     return(
         <div className="bs-banner">
             <div className="banner__innhold">
-                <Systemtittel tag="h1">Info om meg</Systemtittel>
+                <Systemtittel tag="h1">Veiledningsinformasjon</Systemtittel>
             </div>
         </div>
     );
