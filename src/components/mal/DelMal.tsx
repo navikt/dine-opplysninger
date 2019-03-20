2224e8157bff00784d7c8c6370d67d7b50404403
Joselito Poblete <joselito.poblete@gmail.com>
2019-03-20 16:14:20 +0100

    FO-2044 Justering etter ffv

--- src/components/mal/DelMal.tsx
+++ src/components/mal/DelMal.tsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 import './DelMal.less';
 import Systemtittel from 'nav-frontend-typografi/lib/systemtittel';
 import Normaltekst from 'nav-frontend-typografi/lib/normaltekst';
-import { useEffect, useState } from 'react';
+import { Dispatch, useEffect, useState } from 'react';
 import { hentMal, oppdaterMal } from '../../api/api';
 import Textarea from 'nav-frontend-skjema/lib/textarea';
 import { MouseEventHandler } from 'react';
+import { SetStateAction } from 'react';
 
 export interface MalType {
     mal: string;
@@ -32,56 +33,29 @@ function DelMal () {
     if (feilState) {
         return <div className="mal-container">Oops det skjedde en feil ...</div>;
     }
-    
+
+    const visningAvDelMal = () => {
+        return !skalEndreState
+            ?
+            <VisDelMal malState={malState} setSkalEndreState={setSkalEndreState}/>
+            :
+            <RedigerDelMal malState={malState} setMalState={setMalState} setSkalEndreState={setSkalEndreState}/>;
+    };
+
     return(
         <div className="del-mal">
             {
-                !skalEndreState
-                ?
-                    (
-                        <>
-                            <Systemtittel className="del-mal-tittel">Forklaring og delmål</Systemtittel>
-                            <Normaltekst className="del-mal-beskrivelse">{malState}</Normaltekst>
-                            <DelMalKnapp
-                                tekst="Endre"
-                                onClick={(e: React.MouseEvent<HTMLElement>) => {
-                                    e.preventDefault();
-                                    setSkalEndreState(true);
-                                }}
-                            />
-                        </>
-                    )
-                    :
-                    (
-                        <>
-                            <Textarea
-                                textareaClass="typo-normal"
-                                value={malState}
-                                onChange={(e) => {
-                                    const mal = (e.target as HTMLInputElement).value;
-                                    setMalState(mal);
-                                }}
-                                label={<Systemtittel className="del-mal-tittel">Forklaring og delmål</Systemtittel>}
-                            />
-                            <DelMalKnapp
-                                tekst="Lagre"
-                                onClick={(e: React.MouseEvent<HTMLElement>) => {
-                                    e.preventDefault();
-                                    oppdaterMal(malState)
-                                        .then(() => {
-                                            setSkalEndreState(false);
-                                        });
-                                }}
-                            />
-                        </>
-                    )
-
+                visningAvDelMal()
             }
         </div>
     );
 }
 
 export default DelMal;
+
+/*************
+* Kompenter
+*************/
 
 interface DelMalKnappType {
     tekst: string;
@@ -97,5 +71,56 @@ const DelMalKnapp = ({tekst, onClick}: DelMalKnappType) => {
         >
             {tekst}
         </a>
+    );
+};
+
+interface VisDelMalProps {
+    malState: string;
+    setSkalEndreState: Dispatch<SetStateAction<boolean>>;
+}
+const VisDelMal = (props: VisDelMalProps) => {
+    return (
+        <>
+            <Systemtittel className="del-mal-tittel">Forklaring og delmål</Systemtittel>
+            <Normaltekst className="del-mal-beskrivelse">{props.malState}</Normaltekst>
+            <DelMalKnapp
+                tekst="Endre"
+                onClick={(e: React.MouseEvent<HTMLElement>) => {
+                    e.preventDefault();
+                    props.setSkalEndreState(true);
+                }}
+            />
+        </>
+    );
+};
+
+interface RedigerDelMal {
+    malState: string;
+    setMalState: Dispatch<SetStateAction<string>>;
+    setSkalEndreState: Dispatch<SetStateAction<boolean>>;
+}
+const RedigerDelMal = (props: RedigerDelMal) => {
+    return (
+        <>
+            <Textarea
+                textareaClass="typo-normal"
+                value={props.malState}
+                onChange={(e) => {
+                    const mal = (e.target as HTMLInputElement).value;
+                    props.setMalState(mal);
+                }}
+                label={<Systemtittel className="del-mal-tittel">Forklaring og delmål</Systemtittel>}
+            />
+            <DelMalKnapp
+                tekst="Lagre"
+                onClick={(e: React.MouseEvent<HTMLElement>) => {
+                    e.preventDefault();
+                    oppdaterMal(props.malState)
+                        .then(() => {
+                            props.setSkalEndreState(false);
+                        });
+                }}
+            />
+        </>
     );
 };
