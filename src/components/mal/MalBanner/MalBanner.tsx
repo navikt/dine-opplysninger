9fd13b3d7fc3e46c5f0e79a72a2e028b5f6b72a9
Joselito Poblete <joselito.poblete@gmail.com>
2019-04-29 14:18:59 +0200

    FO-2184 Mulig å skjule MalBanner

--- src/components/mal/MalBanner/MalBanner.tsx
+++ src/components/mal/MalBanner/MalBanner.tsx
@@ -3,15 +3,25 @@ import './MalBanner.less';
 import InfoSvg from './svg/info';
 import Normaltekst from 'nav-frontend-typografi/lib/normaltekst';
 import Lukknapp from 'nav-frontend-lukknapp';
+import { useState } from 'react';
 function MalBanner () {
+    const [vis, setVis] = useState(true);
 
+    if (!vis) {
+        return null;
+    }
     return(
         <div className="mal-banner">
             <InfoSvg/>
             <Normaltekst className="mal-banner-beskrivelse">
                 Beskriv målet ditt og hva du mener må til for å nå det. Da kan veilederen din gi deg riktigere veiledning.
             </Normaltekst>
-            <Lukknapp className="mal-banner-lukkknapp"/>
+            <Lukknapp
+                className="mal-banner-lukkknapp"
+                onClick={() => {
+                    setVis(!vis);
+                }}
+            />
         </div>
     );
 }
