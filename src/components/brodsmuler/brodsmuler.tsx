e82a00d967c25dfaaffc751ae10bebcbbb4ac2d1
Joselito Poblete <joselito.poblete@gmail.com>
2019-05-07 15:47:56 +0200

    FO-2169 Legg til brodsmule

---
+++ src/components/brodsmuler/brodsmuler.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import Lenke from 'nav-frontend-lenker';
+import personSvg from './person.svg';
+import './brodsmuler.less';
+
+const DITTNAV_PATH = '/dittnav';
+const VEIENTILARBEID_PATH = '/veientilarbeid';
+
+const Brodsmuler: React.FunctionComponent = () => {
+    return (
+        <div className="brodsmuler">
+            <img src={personSvg} alt="person-illustrasjon" className="brodsmuler__illustrasjon"/>
+            <ol className="brodsmuler__list">
+                <li className="brodsmuler__item typo-normal">
+                    <Lenke href={DITTNAV_PATH}>
+                        Ditt NAV
+                    </Lenke>
+                </li>
+                <li className="brodsmuler__item typo-normal">
+                    <Lenke href={VEIENTILARBEID_PATH}>
+                        Veiledning
+                    </Lenke>
+                </li>
+                <li className="brodsmuler__item typo-normal">
+                    Veiledningsinformasjon
+                </li>
+            </ol>
+        </div>
+    );
+};
+
+export default Brodsmuler;
