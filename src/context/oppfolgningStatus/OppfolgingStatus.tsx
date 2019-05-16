9e5b3683ec6452811e4b8ea55c0c2c313a9abfb0
Joselito Poblete <joselito.poblete@gmail.com>
2019-05-16 11:20:31 +0200

    FO-2221 Vise lenke til registrering, dersom brukere ikke undr.oppf

--- src/context/oppfolgningStatus/OppfolgingStatus.tsx
+++ src/context/oppfolgningStatus/OppfolgingStatus.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 import { hentOppfolgingStatus, OppfolgingStatusType } from '../../api/api';
 import DataFetcher from '../../utils/dataFetcher';
-import AlertStripe from 'nav-frontend-alertstriper';
+import AlertStripeAdvarsel from 'nav-frontend-alertstriper/lib/advarsel-alertstripe';
+import Lenke from 'nav-frontend-lenker';
+import HoyreChevron from 'nav-frontend-chevron/lib/hoyre-chevron';
+
+const ARBEIDSSOKERREGISTRERING_URL = '/arbeidssokerregistrering';
 
 interface OppfolgingStatusProps {
     children: React.ReactNode;
@@ -14,9 +18,10 @@ function OppfolgingStatus(props: OppfolgingStatusProps) {
                 if (!data.underOppfolging) {
                     return (
                         <div id="ikke-under-oppfolgning-container">
-                            <AlertStripe type="info" className="ikke-under-oppfolgning-boks">
-                                Du er ikke under arbeidsrettet oppfølgning.
-                            </AlertStripe>
+                            <AlertStripeAdvarsel className="ikke-under-oppfolgning-boks">
+                                Du må være registrert hos NAV for å se informasjon om deg.
+                                <br/> <Lenke href={ARBEIDSSOKERREGISTRERING_URL}>Registrer deg hos NAV <HoyreChevron/></Lenke>
+                            </AlertStripeAdvarsel>
                         </div>
                     );
                 }
