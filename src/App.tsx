0a52c51dc42321e28782f1d5624f717d94d6d6d6
Joselito Poblete <joselito.poblete@gmail.com>
2019-05-23 13:34:22 +0200

    FO-2234

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Mal from './components/mal/Mal';
 import EndreRegistreringerMock from './mock/EndreRegistreringerMock';
 import Brodsmuler from './components/brodsmuler/brodsmuler';
 import Hensyn from './components/hensyn/hensyn';
+import TimoutboxModal from 'nav-fo-timeoutbox-modal/dist/TimoutboxModal';
 
 interface State {}
 
@@ -20,6 +21,7 @@ class App extends React.Component<AppProps, State> {
     render() {
         return (
             <AppProviders>
+                <TimoutboxModal/>
                 <EndreRegistreringerMock/>
                 <Brodsmuler/>
                 <VeilederBanner/>
