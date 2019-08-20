e967fad2a1ce23278b966d1bfe009ba9c4a363ee
Alexander Gård <alexander.gard@nav.no>
2019-08-20 12:44:57 +0200

    KAIZEN-0 La til støtte for context-path på heroku

--- heroku-server.js
+++ heroku-server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
+const pkg = require('./package.json');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use('/', express.static(path.join(__dirname, 'build')));
+app.use(pkg.homepage || '/', express.static(path.join(__dirname, 'build')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/build/index.html'));
